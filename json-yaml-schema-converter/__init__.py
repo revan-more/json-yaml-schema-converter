4c70f3082b78c5def66f4c035edd833d2e53f72b
Revan More <65714331+revan-more@users.noreply.github.com>
2025-08-11 15:08:16 +0530

    Update __init__.py

--- json-yaml-schema-converter/__init__.py
+++ json-yaml-schema-converter/__init__.py
@@ -1,11 +1,1 @@
 """JSON to YAML Schema Converter Package"""
-
-from .converter import JSONToYAMLConverter
-
-__version__ = "0.1.0"
-__author__ = "Revan More"
-__email__ = "revanmore@45gmail.com"
-__description__ = """Convert JSON API responses to YAML schema
-                    with format with properties, types, and titles"""
-
-__all__ = ["JSONToYAMLConverter"]
